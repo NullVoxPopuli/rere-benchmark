09087e93228fee111c107f19338572710b34fc06
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-02-05 20:51:03 -0500

    I don't know how to fix solid rn

--- frameworks/solid/ten-k-items-one-time/src/App.tsx
+++ frameworks/solid/ten-k-items-one-time/src/App.tsx
@@ -1,23 +1,54 @@
-import { createEffect } from 'solid-js'
+import { createEffect, For, createSignal } from 'solid-js'
 import { helpers } from 'common';
-import { createStore } from "solid-js/store"
+import { createStore } from 'solid-js/store';
 
 function App() {
-  const [items, setItems] = createStore(Array(10_000));
+  // const [items, setItems] = createSignal(Array(10_000));
+  //
+  // createEffect(() => {
+  //   helpers['10ki1u'].run((i) => {
+  //     setItems((previous) => {
+  //       let replacement = previous.map((item, index) => {
+  //         return index === i ? i : item;
+  //       });
+  //       return replacement;
+  //     });
+  //   });
+  // }, [])
+  //
+  // return <>{items().map(i => `${i} `)}</>;
+
+  // const [store, setStore] = createStore({ items: Array(10_000) });
+
+  // createEffect(() => {
+  //   helpers['10ki1u'].run((i) => {
+  //     setStore('items', (previous) => {
+  //       let replacement = previous.map((item, index) => {
+  //         return index === i ? i : item;
+  //       });
+  //       return replacement;
+  //     });
+  //   });
+  // });
+  //
+  // return <For each={store.items}>{(i => `${i} `)}</For>
+
+  const [store, setStore] = createStore({ items: Array(10_000) });
 
   createEffect(() => {
-    helpers['10ki1u'].run((i) => {
-      items[i] = i;
-      setItems(items);
-    });
-  }, [])
+    requestAnimationFrame(() => {
+      helpers['10ki1u'].run((i) => {
+        setStore('items', (previous) => {
+          let replacement = previous.map((item, index) => {
+            return index === i ? i : item;
+          });
+          return replacement;
+        });
+      });
+    })
+  });
 
-
-  return <>{items.map(i => {
-    return <>
-      {i}
-    </>;
-  })}</>
+  return <For each={store.items}>{(i => `${i} `)}</For>
 }
 
 export default App
