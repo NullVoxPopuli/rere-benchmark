889f7d869fc563cc01e144b088cbc849f8b29041
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-02-28 22:16:27 -0500

    Fix

--- frameworks/react/dbmon-with-chat/vite.config.ts
+++ frameworks/react/dbmon-with-chat/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, searchForWorkspaceRoot } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
+  server: {
+    fs: {
+      allow: [searchForWorkspaceRoot(process.cwd()), `../../../common`],
+    },
+  },
 })
--- frameworks/solid/dbmon-with-chat/vite.config.ts
+++ frameworks/solid/dbmon-with-chat/vite.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, searchForWorkspaceRoot } from 'vite'
 import solid from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solid()],
+  server: {
+    fs: {
+      allow: [searchForWorkspaceRoot(process.cwd()), `../../../common`],
+    },
+  },
 })
